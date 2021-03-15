6c4d85b46d4fce26b856204d5188d7f897746741
G433N <44841986+G433N@users.noreply.github.com>
2021-03-15 09:13:19 +0100

    Update SudokuBruteSolver.java

--- src/com/green/sudoku/solvers/SudokuBruteSolver.java
+++ src/com/green/sudoku/solvers/SudokuBruteSolver.java
@@ -4,32 +4,14 @@ import java.util.ArrayList;
 
 import com.green.sudoku.math.ArrayLooper2D;
 
-public class SudokuBruteSolver { // 
-	
-	/*	TODO SudokuBruteSolver planning
-	 *  Create a 2D Array of ArrayList with possible values
-	 *  Loop thru sheet and get all starting possibilities and deep copy them.
-	 *  
-	 *  Start a recursive loop for i between 1 -> size^2. Use ArrayLooper2D to get coordinates.
-	 *  
-	 *  The loop should be able to move forward and backwards and to add and remove from the possibilities
-	 *  
-	 *  If i == 0   throw error probably unsolvable 
-	 *  If i == size^2 + 1	Sudoku is probably solved // It works in my head at least
-	 *  
-	 *  Make 2 functions for column, row, box = x
-	 *  
-	 *  Remove possibility from every x
-	 *  Add possibility from every x
-	 *  
-	 */
+public class SudokuBruteSolver {
 	
 	/* TODO SudokuBruteSolver roadmap	
 	 * 
 	 */
 	
 	final int sheetSize;
-	final int boxSize = 3; // TODO Add to constructor
+	final int boxSize = 3;
 	Cell[][] cells;
 	Cell[][] cellsCopy;
 	
